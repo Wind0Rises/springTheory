249fb9eb480de60272d92b627fe79693ce140e61
aslangas <1474673858@qq.com>
2020-05-16 13:38:04 +0800

    aop

---
+++ 001_first_test/001-first-test/src/main/java/com/liu/study/spring/aop/CustomAspect.java
@@ -0,0 +1,17 @@
+package com.liu.study.spring.aop;
+
+/**
+ * @desc 
+ * @author Liuweian
+ * @createTime 2020/5/15 21:08
+ * @version 1.0.0
+ */
+public class CustomAspect {
+    public void before() {
+        System.out.println("CustomAspect ---  前置处理");
+    }
+
+    public void after() {
+        System.out.println("CustomAspect ---  后置处理");
+    }
+}

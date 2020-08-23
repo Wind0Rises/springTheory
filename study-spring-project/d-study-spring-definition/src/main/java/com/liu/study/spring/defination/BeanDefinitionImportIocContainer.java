93690e3e314e629a3f9afefde7a1b9ef392bd9f9
aslangas <1474673858@qq.com>
2020-08-23 22:13:38 +0800

    知识定义了类，没有实现

---
+++ study-spring-project/d-study-spring-definition/src/main/java/com/liu/study/spring/defination/BeanDefinitionImportIocContainer.java
@@ -0,0 +1,24 @@
+package com.liu.study.spring.defination;
+
+/**
+ * @desc 
+ * @author Liuweian
+ * @createTime 2020/8/23 20:41
+ * @version 1.0.0
+ */
+public class BeanDefinitionImportIocContainer {
+
+    /**
+     * XMl配置元信息，
+     * java注解配置元信息：@Bean、@Component、@Import
+     * Java Api配置元信息：BeanDefinitionRegister.registerBeanDefinition、BeanDefinitionRegisterUtils
+     *                    AnnotationBeanDefinitionReader。
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+}

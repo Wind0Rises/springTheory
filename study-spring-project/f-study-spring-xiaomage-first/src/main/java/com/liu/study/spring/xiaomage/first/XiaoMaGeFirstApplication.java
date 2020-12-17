a0b0d231861253d478af14c2156d75e706757f01
liuweian <1qaz2wsx>
2020-12-17 15:00:51 +0800

    xiaomage first

---
+++ study-spring-project/f-study-spring-xiaomage-first/src/main/java/com/liu/study/spring/xiaomage/first/XiaoMaGeFirstApplication.java
@@ -0,0 +1,14 @@
+package com.liu.study.spring.xiaomage.first;
+
+/**
+ * @author lwa
+ * @version 1.0.0
+ * @createTime 2020/12/17 14:57
+ */
+public class XiaoMaGeFirstApplication {
+
+    public static void main(String[] args) {
+        
+    }
+
+}

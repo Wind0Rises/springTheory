11314899d608eab228b9e7a2fa141119e036298d
aslangas <1474673858@qq.com>
2021-01-10 14:15:27 +0800

    Bean Definition学习

---
+++ study-spring-project/h-study-spring-xiaomage-dependency-lookup/src/main/java/com/liu/study/spring/dependecy/lookup/ObjectProviderDemo.java
@@ -0,0 +1,35 @@
+package com.liu.study.spring.dependecy.lookup;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 
+ * @author Liuweian
+ * @createTime 2020/12/26 22:00
+ * @version 1.0.0
+ */
+public class ObjectProviderDemo {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(ObjectProviderDemo.class);
+        applicationContext.refresh();
+
+        lookupByObjectProvider(applicationContext);
+
+        applicationContext.close();
+    }
+
+    @Bean
+    public String getHelloWorld() {
+        return "Hello World!!";
+    }
+
+    private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> beanProvider = applicationContext.getBeanProvider(String.class);
+        System.out.println(beanProvider.getObject());
+    }
+
+}

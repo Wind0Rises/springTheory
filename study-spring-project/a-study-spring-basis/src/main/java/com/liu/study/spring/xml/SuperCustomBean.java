8edec532182ef531bb8e3e504976c788ededa982
liuweian <1qaz2wsx>
2021-01-13 09:02:16 +0800

    基础的学习

---
+++ study-spring-project/a-study-spring-basis/src/main/java/com/liu/study/spring/xml/SuperCustomBean.java
@@ -0,0 +1,15 @@
+package com.liu.study.spring.xml;
+
+import lombok.Data;
+
+/**
+ * @author lwa
+ * @version 1.0.0
+ * @createTime 2021/1/12 18:09
+ */
+@Data
+public class SuperCustomBean extends CustomBean {
+
+    private Integer age;
+
+}
